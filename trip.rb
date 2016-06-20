5bc64680d78c7ac6181c7c05eb3403d7fd61d459
Susan Wie <susanhwie@gmail.com>
2016-06-20 18:40:08 +0000

    Done.

---
+++ trip.rb
@@ -0,0 +1,13 @@
+puts "Where would you like to stay?"
+place = gets.chomp()
+
+puts "Which sites would you like to visit?"
+sites = gets.chomp()
+
+puts "What do you want to eat?"
+food = gets.chomp()
+
+puts "How many nights do you want to stay?"
+nights = gets.chomp()
+
+puts "You want to go to #{place}, visit #{sites}, eat #{food}, and stay for #{nights} nights."
